d92597d59248544582f56d9ec485034ffeb78fcd
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2019-01-09 09:45:09 +0100

    Nix files

---
+++ mqtt/mqtt.nix
@@ -0,0 +1,26 @@
+{ mkDerivation, aeson, async, base, containers, hpack, influxdb
+, lens, mqtt-hs, mtl, stdenv, stm, text, time, uuid
+}:
+mkDerivation {
+  pname = "mqtt";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    aeson async base containers influxdb lens mqtt-hs mtl stm text time
+    uuid
+  ];
+  libraryToolDepends = [ hpack ];
+  executableHaskellDepends = [
+    aeson async base containers influxdb lens mqtt-hs mtl stm text time
+    uuid
+  ];
+  testHaskellDepends = [
+    aeson async base containers influxdb lens mqtt-hs mtl stm text time
+    uuid
+  ];
+  preConfigure = "hpack";
+  homepage = "https://github.com/atopuzov/mqtt#readme";
+  license = stdenv.lib.licenses.bsd3;
+}
