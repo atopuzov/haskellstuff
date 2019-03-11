4dd08b41d1855e84945d729c6661d7e333570c5e
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2019-03-11 22:59:23 +0000

    Dublin bikes

---
+++ bikes/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for bikes
+
+## Unreleased changes
---
+++ bikes/src/Lib.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE OverloadedStrings          #-}
+
+module Lib
+    ( someFunc, Station(..)
+    ) where
+
+import Data.Aeson (decodeStrict, parseJSON, withObject, (.:), (.=))
+import Data.Aeson.Types (FromJSON)
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS
+import Network.HTTP.Simple
+
+-- {"number":42,
+--  "contract_name":"Dublin",
+--  "name":"SMITHFIELD NORTH",
+--  "address":"Smithfield North",
+--  "position":{"lat":53.349562,"lng":-6.278198},
+--  "banking":true,
+--  "bonus":false,
+--  "bike_stands":30,
+--  "available_bike_stands":1,
+--  "available_bikes":29,
+--  "status":"OPEN",
+--  "last_update":1552330831000},
+
+data Station = Station {
+    number :: Int
+  , bikeStands :: Int
+  , availableBikes :: Int
+  , availableBikeStands :: Integer
+                          } deriving Show
+
+instance FromJSON Station where
+  parseJSON = withObject "Station" $ \o -> do
+    number              <- o .: "number"
+    bikeStands          <- o .: "bike_stands"
+    availableBikes      <- o .: "available_bikes"
+    availableBikeStands <- o .: "available_bike_stands"
+    return Station{..}
+
+
+someFunc :: IO ()
+someFunc = do
+  manager <- newManager tlsManagerSettings
+
+  request <- parseRequest "https://api.jcdecaux.com"
+  let request'
+        = setRequestMethod "GET"
+          $ setRequestPath "/vls/v1/stations"
+          $ setRequestQueryString [ ("contract", Just "Dublin")
+                                  , ("apiKey", Just "a360b2a061d254a3a5891e4415511251899f6df1")
+                                  ]
+          $ request
+
+  response <- httpLBS request'
+
+  putStrLn $ "The status code was: " ++ show (getResponseStatusCode response)
+  putStrLn $ show (getResponseBody response)
