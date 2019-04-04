484c91e1bf35d1333e92c3da074a8f695496e8f7
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2019-04-04 22:16:23 +0100

    Monad transformer fun

--- baastad/1and2and3-eval.hs
+++ baastad/1and2and3-eval.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 import           Line
 import           Term
 
@@ -5,7 +6,17 @@ import           Control.Monad.Trans.Class  (lift)
 import           Control.Monad.Trans.State  (StateT, get, put, runStateT)
 import           Control.Monad.Trans.Writer (WriterT, runWriterT, tell)
 
-tick = get >>= \x -> put (x + 1)
+import           Control.Monad.Trans.State  (StateT, runStateT)
+import           Control.Monad.Trans.Writer (WriterT, runWriterT)
+
+import           Control.Monad.Except       (MonadError, throwError)
+import qualified Control.Monad.State        as MS (MonadState, get, put)
+import qualified Control.Monad.Writer       as MW (MonadWriter, tell)
+
+import           Data.Functor.Identity
+
+
+tick = get >>= put . (+1)
 
 eval :: Term -> WriterT Output (StateT Int (Either String)) Int
 eval (Con a) = do
@@ -22,7 +33,8 @@ eval (Div t u) = do
 runOk =   runStateT (runWriterT (eval (Term.answer))) 0
 runFail = runStateT (runWriterT (eval (Term.error)))  0
 
-tick' = get >>= \x -> put (x + 1)
+
+tick' = get >>= put . (+1)
 
 eval' :: Term -> StateT Int (WriterT Output (Either String)) Int
 eval' (Con a) = do
@@ -38,3 +50,24 @@ eval' (Div t u) = do
 
 runOk' =   runWriterT (runStateT (eval' Term.answer) 0)
 runFail' = runWriterT (runStateT (eval' Term.error)  0)
+
+
+tick'' :: (MS.MonadState Int m) => m ()
+tick'' = MS.get >>= MS.put . (+1)
+
+eval'':: (MW.MonadWriter Output m, MS.MonadState Int m, MonadError String m) => Term -> m Int
+eval'' (Con a) = return a
+eval'' (Div t u) = do
+  a <- eval'' t
+  b <- eval'' u
+  tick''
+  MW.tell $ line (Div t u) (a `div` b)
+  if b == 0
+    then throwError "divide by zero"
+    else return (a `div` b)
+
+type T1 = WriterT Output (StateT Int (Either String)) Int
+type T2 = StateT Int (WriterT Output (Either String)) Int
+
+runOkT1 = runStateT  (runWriterT ((eval'' Term.answer)::T1)) 0
+runOkT2 = runWriterT (runStateT  ((eval'' Term.answer)::T2)  0)
