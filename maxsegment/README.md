9421e9798d55ba7a15ef71a792668f3fd5825085
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2019-10-29 19:20:14 +0000

    Initial commit

---
+++ maxsegment/README.md
@@ -0,0 +1,6 @@
+
+# maxsegment
+
+An attempt to do the max segments from [1] to understand monoid homomorphism
+
+[1] http://cyberglot.space/talks/lambdaworld-april.pdf
---
+++ maxsegment/app/Main.hs
@@ -0,0 +1,48 @@
+module Main where
+
+import Lib
+
+import Data.List
+import Data.Monoid
+
+-- nums :: [Int]
+nums = [31, -41, 59, 26, -53, 58, 97, -93, -23, 84]
+
+inits' = tail . inits
+tails' = init . tails
+segs = concat . map tails' . inits'
+
+sol1 = maximum . map sum . segs
+
+-- We replace segs with it's definition
+sol2 = maximum . map sum . concat . map tails' . inits'
+
+-- Push sum under concat
+-- map sum . concat = concat . map (map sum)
+sol3 = maximum . concat . map (map sum) . map tails' . inits'
+
+-- maximum . concat = maximum . map maximum
+sol4 = maximum . map maximum . map (map sum) . map tails' . inits'
+
+-- map (map sum) . map tails' = map (map sum . tails')
+-- map f . map g = map (f . g)
+sol5 = maximum . map maximum . map (map sum . tails') . inits'
+
+-- map maximum . map (map sum . tails') = map (maximum . (map sum . tails'))
+-- map f . map g = map (f . g)
+sol6 = maximum . map (maximum . map sum . tails') . inits'
+
+d = -1000 -- minBound -- -Infinity
+e = 0
+
+sol7 = maximum . map ((foldl (\x y -> if x > y then x else y) d) . map (foldl (\x y -> x + y) e) . tails') . inits'
+sol8 = maximum . map ((foldl max d) . map (foldl (+) e) . tails') . inits'
+--                         S        . map      T        . tails'
+-- S = Max, max,  d = -Infinity
+-- T = Sum, plus, d = 0
+
+-- o x y = (max x d) + y ???
+-- sol9 = maximum . map (foldr o d) . inits' ???
+
+main :: IO ()
+main = someFunc
