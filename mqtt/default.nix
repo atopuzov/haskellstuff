3fcc623c1fdac1e70e5bc14f39d16b2a1e735e8f
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2020-01-03 14:21:49 +0000

    Make it work with GHC 8.6.5
    * Use my mqtt-hs fork with fixes for ghc 8.6.5

--- mqtt/default.nix
+++ mqtt/default.nix
@@ -1,7 +1,20 @@
-{ pkgs ? import <nixpkgs> {}
+{ pkgs ? import <nixpkgs> {}, compiler ? "ghc865"
 }:
-
 let
-  stack-pkgs = pkgs.callPackage ./nix {};
+  overrides = {
+    overrides = self: super: {
+      mqtt-hs = pkgs.haskell.lib.overrideCabal super.mqtt-hs (drv: {
+        src = pkgs.fetchFromGitHub {
+          owner = "atopuzov";
+          repo = "mqtt-hs";
+          rev = "5eb90ddbbddf14c6a02e5a2257a058a8c36b3731";
+          sha256 = "0c1c3mjsh36cpsyg4hisqzhxd3sglidmnmizqjg19swzp5kwf7p8";
+        };
+        broken = false;
+      });
+    };
+  };
+  haskellPackages = pkgs.haskell.packages.${compiler}.override overrides;
+  drv = haskellPackages.callPackage ./mqtt.nix { };
 in
-  stack-pkgs.mqtt
+if pkgs.lib.inNixShell then drv.env else drv
