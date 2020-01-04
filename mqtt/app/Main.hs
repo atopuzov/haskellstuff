c99d03e602b047822a7d9f5ca1f71f8722080f81
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2020-01-04 14:04:07 +0000

    Support for command line arguments
     * MQTT params
     * InfluxDB params

--- mqtt/app/Main.hs
+++ mqtt/app/Main.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE ApplicativeDo              #-}
 {-# LANGUAGE TypeFamilies               #-}
 -- {-# LANGUAGE NoImplicitPrelude          #-}
 module Main where
@@ -13,6 +14,7 @@ import           Lib
 import           Control.Concurrent       (forkIO)
 import           Control.Concurrent.Async (async)
 import           Control.Concurrent.STM   (newTChanIO, readTChan)
+import qualified Control.Exception        as E
 import           Control.Lens             ((&), (.~), (?~))
 import           Control.Monad            (forever, void)
 import           Control.Monad.Except     (ExceptT, MonadError, runExceptT)
@@ -23,26 +25,35 @@ import           Control.Monad.STM        (atomically)
 import           Data.Aeson               (decodeStrict, parseJSON, withObject,
                                            (.:), (.=))
 import           Data.Aeson.Types         (FromJSON)
+import qualified Data.Map                 as Map
 import           Data.Maybe               (fromJust)
 import           Data.Text                (Text)
 import           Data.Time.Clock          (UTCTime)
-import           System.Exit              (exitFailure)
-import           System.IO                (BufferMode (NoBuffering), hPutStrLn,
-                                           hSetBuffering, stderr, stdout)
-
-import qualified Control.Exception        as E
-import qualified Data.Map                 as Map
 import qualified Data.UUID                as UUID
 import qualified Data.UUID.V1             as UUID.V1
 import qualified Database.InfluxDB        as InfluxDB
 import qualified Database.InfluxDB.Types  as InfluxDB.Types
 import qualified Network.MQTT             as MQTT
 import qualified Network.MQTT.Types       as MQTT.Types
+import qualified Options.Applicative      as OA
+import           System.Exit              (exitFailure)
+import           System.IO                (BufferMode (NoBuffering), hPutStrLn,
+                                           hSetBuffering, stderr, stdout)
+
+data CliOptions = CliOptions {
+    _mqttHost       :: String
+  , _mqttPort       :: String
+  , _mqttUsername   :: Text
+  , _mqttPassword   :: Text
+  , _mqttTopic      :: Text
+  , _influxHost     :: Text
+  , _influxDatabase :: Text
+  }
 
 data AppOptions = AppOptions {
-  mqttConfig  :: MQTT.Config
-  , mqttTopic :: MQTT.Types.Topic
-  , influxWp  :: InfluxDB.WriteParams
+    mqttConfig  :: MQTT.Config
+  , mqttTopic   :: MQTT.Types.Topic
+  , influxWp    :: InfluxDB.WriteParams
   }
 
 type AppConfig = MonadReader AppOptions
@@ -90,23 +101,47 @@ renderError (IOError e) = do
     putStrLn "There was an error:"
     putStrLn $ "  " ++ show e
 
+cliParser :: OA.Parser CliOptions
+cliParser = do
+  _mqttHost <- OA.option OA.str
+    (OA.long "mqtt-host" <> OA.value "localhost" <> OA.help "MQTT host to connect to")
+  _mqttPort <- OA.option OA.str
+    (OA.long "mqtt-port" <> OA.value "1883" <> OA.help "MQTT port to connect to")
+  _mqttUsername <- OA.option OA.str
+    (OA.long "mqtt-username" <> OA.short 'u' <> OA.help "MQTT username")
+  _mqttPassword <- OA.option OA.str
+    (OA.long "mqtt-password" <> OA.short 'p' <> OA.help "MQTT password")
+  _mqttTopic <- OA.option OA.str
+    (OA.long "mqtt-topic" <> OA.value "outTopic" <> OA.help "MQTT topic")
+  _influxHost <- OA.option OA.str
+    (OA.long "influx-host" <> OA.value "localhost" <> OA.help "InfluxDB host to connect to")
+  _influxDatabase <- OA.option OA.str
+    (OA.long "influx-db" <> OA.value "temperature" <> OA.help "InfluxDB database")
+  pure CliOptions {..}
+
+cliParserInfo :: OA.ParserInfo CliOptions
+cliParserInfo = OA.info (OA.helper <*> cliParser) (mempty)
+
 main :: IO ()
 main = do
+  opts <- OA.execParser cliParserInfo
+
   hSetBuffering stdout NoBuffering
   cmds <- MQTT.mkCommands
   chan <- newTChanIO
 
   clientId <- UUID.V1.nextUUID
-  let config = (MQTT.defaultConfig cmds chan) {
-                                                  MQTT.cHost = "dubpi.local"
-                                                , MQTT.cClientID = UUID.toText $ fromJust clientId
-                                                , MQTT.cKeepAlive = Just 10
-                                                -- , MQTT.cLogDebug = putStrLn
-                                              }
-
-  let wp = InfluxDB.writeParams (InfluxDB.Types.Database $ "temperature") & InfluxDB.precision .~ InfluxDB.Second
-  let topic = MQTT.Types.toTopic $ MQTT.Types.MqttText "outTopic"
-  let appCfg = AppOptions config topic wp
+  let mqttConfig = (MQTT.defaultConfig cmds chan) {
+          MQTT.cHost      = _mqttHost opts
+        , MQTT.cPort      = read $ _mqttPort opts
+        , MQTT.cClientID  = UUID.toText $ fromJust clientId
+        , MQTT.cKeepAlive = Just 10
+        , MQTT.cUsername  = Just $ _mqttUsername opts
+        , MQTT.cPassword  = Just $ _mqttPassword opts
+        }
+  let influxWp = InfluxDB.writeParams (InfluxDB.Types.Database $ _influxDatabase opts) & InfluxDB.precision .~ InfluxDB.Second
+  let topic = MQTT.Types.toTopic $ MQTT.Types.MqttText $ _mqttTopic opts
+  let appCfg = AppOptions mqttConfig topic influxWp
 
   either renderError return =<< runExceptT (runReaderT (runApp app) appCfg)
 
@@ -124,7 +159,7 @@ app = do
         msg <- fmap decodeMsg $ atomically $ readTChan (MQTT.cPublished config)
         case msg of
           Just value -> do
-            hPutStrLn stdout $ "Received: " ++ show value
+            putStrLn $ "Received: " ++ show value
             runReaderT (writeData value) env
           Nothing -> putStrLn "Unable to decode data."
       _ -> do
@@ -136,4 +171,4 @@ app = do
   -- Network.Socket.getAddrInfo (called with preferred socket type/protocol: AddrInfo {addrFlags = [AI_ADDRCONFIG], addrFamily = AF_UNSPEC, addrSocketType = Stream, addrProtocol = 6, addrAddress = <assumed to be undefined>, addrCanonName = <assumed to be undefined>}, host name: Just "dubpi.local", service name: Just "1883"): does not exist (System error)
   --  (ConnectionFailure Network.Socket.connect: <socket: 13>: does not exist (Connection refused)))
   terminated <- liftIO $ MQTT.run config
-  liftIO $ hPutStrLn stderr $ "Terminated:" ++ show terminated
+  liftIO $ hPutStrLn stderr $ "Application terminated: " ++ show terminated
