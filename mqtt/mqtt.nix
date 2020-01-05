0b8be31e658f085f93342187d134568a59bf932e
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2020-01-05 15:21:45 +0000

    Regenerate mqtt.nix

--- mqtt/mqtt.nix
+++ mqtt/mqtt.nix
@@ -1,26 +1,27 @@
 { mkDerivation, aeson, async, base, containers, hpack, influxdb
-, lens, mqtt-hs, mtl, stdenv, stm, text, time, uuid
+, lens, mqtt-hs, mtl, optparse-applicative, stdenv, stm, text, time
+, uuid
 }:
 mkDerivation {
   pname = "mqtt";
   version = "0.1.0.0";
   src = ./.;
-  isLibrary = false;
+  isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson async base containers influxdb lens mqtt-hs mtl stm text time
-    uuid
+    aeson async base containers influxdb lens mqtt-hs mtl
+    optparse-applicative stm text time uuid
   ];
   libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
-    aeson async base containers influxdb lens mqtt-hs mtl stm text time
-    uuid
+    aeson async base containers influxdb lens mqtt-hs mtl
+    optparse-applicative stm text time uuid
   ];
   testHaskellDepends = [
-    aeson async base containers influxdb lens mqtt-hs mtl stm text time
-    uuid
+    aeson async base containers influxdb lens mqtt-hs mtl
+    optparse-applicative stm text time uuid
   ];
-  # preConfigure = "hpack";
+  prePatch = "hpack";
   homepage = "https://github.com/atopuzov/mqtt#readme";
   license = stdenv.lib.licenses.bsd3;
 }
