c03339a4b1173b780c6bcdd5507bacf8d9ded1c8
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2020-01-06 19:36:38 +0000

    Cleanup

--- mqtt/app/Main.hs
+++ mqtt/app/Main.hs
@@ -103,24 +103,37 @@ renderError (IOError e) = do
 
 cliParser :: OA.Parser CliOptions
 cliParser = do
-  _mqttHost <- OA.option OA.str
-    (OA.long "mqtt-host" <> OA.value "localhost" <> OA.help "MQTT host to connect to")
-  _mqttPort <- OA.option OA.str
-    (OA.long "mqtt-port" <> OA.value "1883" <> OA.help "MQTT port to connect to")
-  _mqttUsername <- OA.option OA.str
-    (OA.long "mqtt-username" <> OA.short 'u' <> OA.help "MQTT username")
-  _mqttPassword <- OA.option OA.str
-    (OA.long "mqtt-password" <> OA.short 'p' <> OA.help "MQTT password")
-  _mqttTopic <- OA.option OA.str
-    (OA.long "mqtt-topic" <> OA.value "outTopic" <> OA.help "MQTT topic")
-  _influxHost <- OA.option OA.str
-    (OA.long "influx-host" <> OA.value "localhost" <> OA.help "InfluxDB host to connect to")
-  _influxDatabase <- OA.option OA.str
-    (OA.long "influx-db" <> OA.value "temperature" <> OA.help "InfluxDB database")
+  _mqttHost <- OA.option OA.str $
+    OA.long "mqtt-host" <>
+    OA.value "localhost" <>
+    OA.help "MQTT host to connect to"
+  _mqttPort <- OA.option OA.str $
+    OA.long "mqtt-port" <>
+    OA.value "1883" <>
+    OA.help "MQTT port to connect to"
+  _mqttUsername <- OA.option OA.str $
+    OA.long "mqtt-username" <>
+    OA.short 'u' <>
+    OA.help "MQTT username"
+  _mqttPassword <- OA.option OA.str $
+    OA.long "mqtt-password" <>
+    OA.short 'p' <>
+    OA.help "MQTT password"
+  _mqttTopic <- OA.option OA.str $
+    OA.long "mqtt-topic" <>
+    OA.value "outTopic" <>
+    OA.help "MQTT topic"
+  _influxHost <- OA.option OA.str $
+    OA.long "influx-host" <>
+    OA.value "localhost" <>
+    OA.help "InfluxDB host to connect to"
+  _influxDatabase <- OA.option OA.str $
+    OA.long "influx-db" <>
+    OA.value "temperature" <> OA.help "InfluxDB database"
   pure CliOptions {..}
 
 cliParserInfo :: OA.ParserInfo CliOptions
-cliParserInfo = OA.info (OA.helper <*> cliParser) (mempty)
+cliParserInfo = OA.info (OA.helper <*> cliParser) mempty
 
 main :: IO ()
 main = do
